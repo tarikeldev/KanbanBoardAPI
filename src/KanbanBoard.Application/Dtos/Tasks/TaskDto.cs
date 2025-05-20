36a927438989fc99cfd886db454aed390326aa26
Tarik El Hajjam <takkofo@gmail.com>
2025-05-20 13:26:44 +0100

    add description

--- src/KanbanBoard.Application/Dtos/Tasks/TaskDto.cs
+++ src/KanbanBoard.Application/Dtos/Tasks/TaskDto.cs
@@ -10,6 +10,7 @@ namespace KanbanBoard.Application.Dtos.Tasks
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public int BoardId { get; set; }
     }
 }
